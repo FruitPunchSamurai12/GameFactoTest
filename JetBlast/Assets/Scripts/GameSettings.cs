716331ec4add0e4b3cd564af2a4b18faf91680b8
FruitPunchSamurai12 <fruitpunchsamuraigr@gmail.com>
2021-10-22 15:09:51 -0700

    first actual commit. set up photon

---
+++ JetBlast/Assets/Scripts/GameSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName ="Manager/GameSettings")]
+public class GameSettings:ScriptableObject
+{
+    [SerializeField]
+    string gameVersion = "0.0.0";
+    public string GameVersion => gameVersion;
+    [SerializeField]
+    string nickName = "Kitsios";
+    public string NickName => nickName;
+}
---
+++ JetBlast/Assets/Scripts/MasterManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName ="Singletons/MasterManager")]
+public class MasterManager:SingletonScriptableObject<MasterManager>
+{
+    [SerializeField]
+    GameSettings gameSettings;
+    public static GameSettings GameSettings => Instance.gameSettings;
+}
